e850be8936f6a60a8c2a821dddbfa8fefc40afd3
unknown <vorakaran83@gmail.com>
2024-08-05 18:44:06 +0530

    badge status work done

--- resources/views/backend/order/index.blade.php
+++ resources/views/backend/order/index.blade.php
@@ -99,22 +99,22 @@
                                 @endif
 
                                     @if($product->is_fulfilled == 0)
-                                        <span class="btn btn-sm btn-warning my-1" style="cursor:unset;">Pending</span>
+                                        <span class="badge bg-warning-light rounded-pill" style="cursor:unset;">Pending</span>
                                     @elseif($product->is_fulfilled == 1)
-                                        <span class="btn btn-sm btn-success my-1" style="cursor:unset;">Approved</span>
+                                        <span class="badge bg-success-light rounded-pill" style="cursor:unset;">Approved</span>
                                     @elseif($product->is_fulfilled == 2)
-                                        <span class="btn btn-sm btn-danger my-1" style="cursor:unset;">Rejected</span>
+                                        <span class="badge bg-danger-light rounded-pill" style="cursor:unset;">Rejected</span>
                                     @elseif($product->is_fulfilled == 4)
-                                        <span class="btn btn-sm btn-danger my-1" style="cursor:unset;">Rejected By Admin</span>
+                                        <span class="badge bg-danger-light rounded-pill" style="cursor:unset;">Rejected By Admin</span>
                                     @elseif($product->is_fulfilled == 5)
-                                        <span class="btn btn-sm btn-info my-1" style="cursor:unset;">Cancelled</span>
+                                        <span class="badge bg-info-light rounded-pill" style="cursor:unset;">Cancelled</span>
                                     @else
-                                    <span class="btn btn-sm btn-warning my-1" style="cursor:unset;">Pending</span>
+                                    <span class="badge bg-warning-light rounded-pill" style="cursor:unset;">Pending</span>
                                 @endif
 
                             @else
 
-                            <span class="btn btn-sm btn-warning my-1" style="cursor:unset;">Pending</span>
+                            <span class="badge bg-warning-light rounded-pill" style="cursor:unset;">Pending</span>
 
                         @endif
                     </td>
